30c0419c78227f2207708a9acf8d20adda04b7ec
Vicious <faekie@hotmail.com>
2011-12-03 15:40:53 +0100

    Changed charset from UTF-8 to ISO-8859-1

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" /><!-- Change charset if needed, also below, meta charset-->
 		<meta name="description" content="XBMC Database Manager" />
 		<meta name="keywords" content="XBMC Database Manager" />
 		<meta name="author" content="Vicious" />
-		<meta charset="utf-8">
+		<meta charset="ISO-8859-1">
 		<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 		<title>XBMC Database Manager</title>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
+		<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
 		<script type="text/javascript">
 			$('#choose').change(function(event) {
 				$('#update').html('This is ' + $('#choose').val() + ' and other info');
 			}); 
-		</script>
+		</script>-->
 	</head>
 	<body>
 		<div id="wrapper">
